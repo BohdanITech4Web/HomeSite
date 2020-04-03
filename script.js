6f788fb152c8f202d1513b0fd21f46ab2d8dedc8
BohdanITech4Web <61540429+BohdanITech4Web@users.noreply.github.com>
2020-04-03 15:57:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
 $(document).ready(function () {
   $(function () {
-    // Owl Carousel
+// Owl Carousel
     var owl = $(".owl-carousel");
     owl.owlCarousel({
-     // centerMode: true,
       items: 3,
       margin: 10,
-      loop: true,
+      loop: false,
       navText: [
-        '<span class="arrow-owl arrow-left"><div></div><img src="img/ready_project/keyboard-right-arrow-button_(4)_копия.png"> </span>',
+        '<span class="arrow-owl arrow-left"><img src="img/ready_project/keyboard-right-arrow-button_(4)_копия.png"> </span>',
         '<span class="arrow-owl arrow-right"><img src="img/ready_project/keyboard-right-arrow-button_(4).png"></span>'
       ],
       nav: true,
       dots: true
-     // rewindNav:false
-   /*   responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            itemToShow: 2,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            itemToShow: 1,
-          }
-        }
-      ]*/
     });
-  });
-
-  $(function () {
     $("#tabs .btn").click(function () {
-      var category = $(this).data("category");
+      var category = $(this).data('owl-filter');
       $("#tabs .btn").removeClass('active');
       $(this).addClass('active');
-      $(".owl-theme.blockreadyproject .item").hide();
+      owl.owlcarousel2_filter( category );
+      /* $(".owl-theme.blockreadyproject .item").hide();
       $(".owl-theme.blockreadyproject ." + category).show();
+      owl.trigger('refreshed.owl.carousel');*/
+    });
+    $('.owl-carousel .owl-dots').each(function() {
+      $(this).appendTo($(this).prev('.owl-nav'));
     })
+    
   });
 });
